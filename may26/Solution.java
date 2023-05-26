325d8450ca868e9fe5f1fadce2ff72649af8efc9
kkkkkdddddhhhhh <133971731+kwon93@users.noreply.github.com>
2023-05-26 16:26:46 +0900

    may26

---
+++ may26/Solution.java
@@ -0,0 +1,24 @@
+package may26;
+
+import java.util.Arrays;
+//정수를 받는다 -> 정수를 2개를 배열에 담는다. -> 오름차순으로 정렬한다
+//-> for문 초기값에 a 조건값에  b를 대입한 뒤 answer 변수에 다 더한다
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        int[] arr = {a,b};
+        Arrays.sort(arr);
+
+        for (int i = arr[0]; i <= arr[1] ; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        long result = s.solution(5,3);
+        System.out.println(result);
+    }
+}
